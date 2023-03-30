0ffaa771653fb32c6ca7042c2e081caf72895684
Shmuel Malikov <sshmoel1@gmail.com>
2023-03-30 06:26:13 +0300

    navbar, loginbar css changes.

--- src/App.js
+++ src/App.js
@@ -5,43 +5,51 @@ function App() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleLogin = (e) => {
-    e.preventDefault();
-    console.log(`Username: ${username} Password: ${password}`);
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Username: ${username}, Password: ${password}`);
   };
 
   return (
     <div className="App">
       <nav>
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-          <li>Login</li>
+          <li>
+            <a href="http://www.google.com">Home</a>
+          </li>
+          <li>
+            <a href="http://www.google.com">About</a>
+          </li>
+          <li>
+            <a href="http://www.google.com">Contact</a>
+          </li>
         </ul>
       </nav>
-      <div className="login-form">
-        <h2>Login Form</h2>
-        <form onSubmit={handleLogin}>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <br />
-          <label htmlFor="password">Password:</label>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={handleUsernameChange} />
+        </label>
+        <br />
+        <label>
+          Password:
           <input
             type="password"
-            id="password"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={handlePasswordChange}
           />
-          <br />
-          <button type="submit">Login</button>
-        </form>
-      </div>
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
