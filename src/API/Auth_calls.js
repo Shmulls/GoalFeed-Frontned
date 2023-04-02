59a68d43023891739e62923f565b276e22e614a5
Shmuel Malikov <sshmoel1@gmail.com>
2023-04-02 20:53:08 +0300

    Route update.

--- src/API/Auth_calls.js
+++ src/API/Auth_calls.js
@@ -3,14 +3,10 @@ import axios from "axios";
 const BASE_URL = "https://sw-project-backend.onrender.com/Auth";
 
 export const sendLoginRequest = async (params) => {
-  const result = await axios
-    .post(`${BASE_URL}/login`, params)
-    .catch((error) => {
-      console.log(error);
-    });
-  if (result && result.status === 200) {
+  const result = await axios.get(BASE_URL, {
+    params,
+  });
+  if (result.status === 200) {
     alert(result.data.message);
   }
 };
-
-/**/
