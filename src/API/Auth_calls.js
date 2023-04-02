a27aa81faaf1e7d75b0bef0e3f9fdb23f916578e
Shmuel Malikov <sshmoel1@gmail.com>
2023-04-02 16:52:17 +0300

    Add exception handler

--- src/API/Auth_calls.js
+++ src/API/Auth_calls.js
@@ -3,9 +3,13 @@ import axios from "axios";
 const BASE_URL = "https://sw-project-backend.onrender.com";
 
 export const sendLoginRequest = async (params) => {
-  const result = await axios.get(BASE_URL, {
-    params,
-  });
+  const result = await axios
+    .get(BASE_URL, {
+      params,
+    })
+    .catch((error) => {
+      console.log(error);
+    });
   if (result.status === 200) {
     alert(result.data.message);
   }
