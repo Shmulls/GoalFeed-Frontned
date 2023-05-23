05abe067f06d5c53972841993f8233dce56fe0af
yuval <yuvalamar15@gmail.com>
2023-05-23 18:16:25 +0300

    base_url change

--- src/back_url.js
+++ src/back_url.js
@@ -1,5 +1,5 @@
 // const BASE_URL = "http://localhost:3001";
-const BASE_URL = "https://sw-project-backend.onrender.com/";
+const BASE_URL = "https://sw-project-backend.onrender.com";
 
 export default BASE_URL;
 //
